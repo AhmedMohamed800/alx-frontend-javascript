7b050552eb8f565067eb034b4c60e01f8f4f61d0
Ahmed Mohamed <ahmedmoh0107@gmail.com>
2024-01-03 17:42:56 +0200

    fix: final user

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,7 +2,7 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.all([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((data) => data)
+    return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+        .then((data) => { console.log(data) })
     .catch((error) => Error(error));
 }
