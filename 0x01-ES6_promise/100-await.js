920eb3f71176018ecb8c09d43ba380e65cb86d38
Ahmed Mohamed <ahmedmoh0107@gmail.com>
2024-01-03 19:03:10 +0200

    fix: handling error in 100-await.js

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,9 +1,14 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
-  const data = {
-    photo: await uploadPhoto().then((photoName) => photoName),
-    user: await createUser().then((userInfo) => userInfo),
-  };
-  return data;
+  let photo;
+  let user;
+  try {
+    photo = await uploadPhoto().then((photoName) => photoName);
+    user = await createUser().then((userInfo) => userInfo);
+  } catch (err) {
+    photo = null;
+    user = null;
+  }
+  return { photo, user };
 }
