d04565c54ac762ba464f07f60b85f9f4fe95a1e6
Ahmed Mohamed <ahmedmoh0107@gmail.com>
2024-01-08 18:43:07 +0200

    Add 8-main

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,10 @@
 export default function cleanSet(sett, startString) {
+  if (startString.length === 0) {
+    return '';
+  }
   const result = [];
   for (const item of sett) {
-    if (item.slice(0, 3) === startString) {
+    if (item.slice(0, startString.length) === startString) {
       result.push(item.slice(startString.length));
     }
   }
