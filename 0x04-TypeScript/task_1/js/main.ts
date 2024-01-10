2227c04fadccd17d6e77eb0c9e9d6c6eb7979a6c
Ahmed Mohamed <ahmedmoh0107@gmail.com>
2024-01-10 14:57:31 +0200

    finish task 1

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -35,6 +35,31 @@ interface printTeacherFunction {
   (firstName: string, lastName: string): string;
 }
 
+
+interface StudentInterface {
+  firstName: string;
+  lastName: string;
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+
+class StudentClass implements StudentInterface {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+  workOnHomework(): string {
+    return "Currently working";
+  }
+  displayName(): string {
+    return this.firstName;
+  }
+}
+
 const printTeacher: printTeacherFunction = function (firstName, lastName) {
   return `${firstName.charAt(0)}. ${lastName}`;
 };
